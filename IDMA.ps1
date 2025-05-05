5026a5b51593af29defd3ce5d75fd78b567a5a69
Mohammad Firoz Kabir <mdfirozkabir.dev@gmail.com>
2025-05-05 14:23:24 +0600

    Added IDMA.PS1 file & uploaded complete Successfully.

---
+++ IDMA.ps1
@@ -0,0 +1,20 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://mdfirozkabir.github.io/IDM-Activator/IDMA-20250505140625.cmd'
+
+
+$FilePath = "$env:TEMP\IDMA.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
